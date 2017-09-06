ae546a5d2610e4c3e207efb42bb69adb47cad559
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-06 18:48:39 +0700

    i

--- i.js
+++ i.js
@@ -1,19 +1,22 @@
 /*
 
-*
-**
-***
-****
+    *
+   **
+  ***
+ ****
 *****
 
 */
 
+// for(let s = '    *', i = 0; ++i <= 5; s = (s += '*').substr(1, 5)) console.log(s);
+
+
 for(let i = 0; i < 5; i++) {
     let str = '';
-    for(let j = 0; j <= i; j++) {
-        str += '*';
+    for(let j = 0; j < 5; j++) {
+        str += (j >= 4 - i) ? '*' : ' ';
     }
     console.log(str);
 }
 
-for(let s = ''; s.length < 6; console.log(s += '*'));
+// for(let s = ''; s.length < 6; console.log(s += '*'));
